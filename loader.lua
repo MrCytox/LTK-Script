391b427e1dc624567c9dd944779a49ba7b4069df
Cytox <137851615+MrCytox@users.noreply.github.com>
2024-04-03 19:42:46 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,1 @@
-
-while task.wait(1) do
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MrCytox/LTK-Script/main/emojitag.lua"))()
-end
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MrCytox/LTK-Script/main/LTK-Script-v1"))()
