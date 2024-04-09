234721404dc5ec6906f23185edbd50495da08f66
Cytox <137851615+MrCytox@users.noreply.github.com>
2024-04-09 15:34:20 -0700

    Update Database.lua

--- Premium/Database.lua
+++ Premium/Database.lua
@@ -8,5 +8,6 @@ function Blacklist(Hwid)
 end
 
 Whitelist("fd95c696a102412d73789c3c362d4f746fc9f75623429d5d5f5e785538eaa156")
+Whitelist("a739a809c3ccf6a0c41a1b2e432ff0d20403e9b7a5f516fb274e1ee7058d89068903c0e4ca96cbe85568b092189622b3")
 
 return Hwids
