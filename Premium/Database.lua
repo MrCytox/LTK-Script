2271037e2daab8230bc98a25105a88c0bd30aad9
Cytox <137851615+MrCytox@users.noreply.github.com>
2024-04-08 23:49:43 -0700

    Update Database.lua

--- Premium/Database.lua
+++ Premium/Database.lua
@@ -1,1 +1,12 @@
+local Hwids = {}
 
+function Whitelist(Hwid)
+    table.insert(Hwids,Hwid)
+end
+function Blacklist(Hwid)
+    table.remove(Hwids, Hwids[Hwid])
+end
+
+Whitelist("fd95c696a102412d73789c3c362d4f746fc9f75623429d5d5f5e785538eaa156")
+
+return Hwids
