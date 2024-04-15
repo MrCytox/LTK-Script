f7ab194f9bdcd9aa80dbb41e8589b3158fb610bb
Cytox <137851615+MrCytox@users.noreply.github.com>
2024-04-15 13:13:03 -0700

    Update emojitag.lua

--- emojitag.lua
+++ emojitag.lua
@@ -1,10 +1,13 @@
+-- credits to swagmode for this btw
 OwnerIDS = {
-    1731926896, -- fortunatesouls main
+    1731926896, -- cytox main [ banned on da hood sadly ]
+    4017019312, -- cytox main alt
+    5709682739, -- cytox sigma alt 2
     4948286244, -- plugs
 }
 
 TesterIDS = {
-    4096995068, -- vic
+    --4096995068, Removed for snaking LTK (W Wlud snake method btw lol)
     4484832617, -- damari
     663588583, -- gtekkau
     3836872720, -- gainedz
@@ -22,25 +25,25 @@ function names()
 		if table.find(OwnerIDS, Player.UserId) then
 			if Player.Character then
 				if Player.Character.Parent.Name == 'Players' then
-					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[Owner - 🛠]' .. Player.DisplayName)
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👓]' .. Player.DisplayName)
 				end
 			end
 		elseif table.find(TesterIDS, Player.UserId) then
 		    if Player.Character then
 				if Player.Character.Parent.Name == 'Players' then
-					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🔌 - Tester]' .. Player.DisplayName)
+					Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[✅]' .. Player.DisplayName)
 				end
 		    end
 		elseif table.find(ltkmember, Player.UserId) then
 		    if Player.Character then
 		        if Player.Character.Parent.Name == 'Players' then
-		            Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👓 - LTK Member]' .. Player.DisplayName)
+		            Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[💎]' .. Player.DisplayName)
 		        end
 		    end
 		    if Player.Character then
 			    if Player.Character.Parent.Name == 'Players' then
 				    if not Player.Character.UpperTorso:FindFirstChild('OriginalSize') then
-					    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[🎮 - LTK User]' .. Player.DisplayName)
+					    Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😃]' .. Player.DisplayName)
 				    end
 			    end
 		    end
