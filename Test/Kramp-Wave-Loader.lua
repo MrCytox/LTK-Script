e945186aee9d85a220f0d1f35f1826e5448b6dc8
Cytox <137851615+MrCytox@users.noreply.github.com>
2024-04-15 13:08:01 -0700

    Update Kramp-Wave-Loader.lua

--- Test/Kramp-Wave-Loader.lua
+++ Test/Kramp-Wave-Loader.lua
@@ -1,5 +1,5 @@
 if identifyexecutor() == "Krampus" then
-  --
+  print("Executer supported")
 elseif identifyexecutor() == 'Wave" then
-  --
+  game.Players.LocalPlayer:Kick("Executer does not support firetouchinterest, or fireclickdetector")
 end
